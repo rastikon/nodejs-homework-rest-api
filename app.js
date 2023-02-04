ada6994482b8130ffed21d4d6b91e20a904f0205
Rostyslav Kononenko <rastikon@gmail.com>
2023-02-05 00:47:50 +0100

    homework_v_1

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+// nqnCrMGxpQQo3oAm
 
-const contactsRouter = require('./routes/api/contacts')
+// mongodb+srv://rostikon:nqnCrMGxpQQo3oAm@cluster0.cks5n2o.mongodb.net/test
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
+// const mongoose = require("mongoose");
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+// const DB_HOST =
+//   "mongodb+srv://rostikon:nqnCrMGxpQQo3oAm@cluster0.cks5n2o.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => console.log("Database connection successful"))
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
 
-app.use('/api/contacts', contactsRouter)
+const contactsRouter = require("./routes/api/contacts");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,145 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const {
+  Contact,
+  joiSchema,
+  joiSchemaUpdate,
+  joiSchemaFavorite,
+} = require("../../models/contact");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// Показати всі контакти
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await Contact.find({});
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result: contacts,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// Знайти контакт по id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await Contact.findById(id);
+    if (!contact) {
+      return null;
+    }
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result: contact,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// Додавання нового контакту
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = joiSchema.validate(req.body);
+    if (error) {
+      error.status = 400;
+      res.json({ message: "missing required name field" });
+      throw error;
+    }
+    const contactAdd = await Contact.create(req.body);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result: contactAdd,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+// Видалення контакту
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await Contact.findByIdAndRemove(id);
+    if (!result) {
+      const { error } = new Error(`Contact with id=${id} not found`);
+      throw error;
+    }
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result: result,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+  // res.json({ message: "contact deleted" });
+});
+
+// Зміна значення поля по id
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = joiSchemaUpdate.validate(req.body);
+    if (error) {
+      error.status = 400;
+      throw error;
+    }
+    const { id } = req.params;
+
+    const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+    if (!result) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.json({ result });
+  } catch (error) {
+    next(error);
+  }
+  // 63dee3d5a549985d05f2b8cd
+});
+
+// Зміна значення одного поля
+router.patch("/:id/favorite", async (req, res, next) => {
+  try {
+    const { error } = joiSchemaFavorite.validate(req.body);
+    if (error) {
+      error.status = 400;
+      throw error({ message: "missing field favorite" });
+    }
+    const { id } = req.params;
+    const { favorite } = req.body;
+
+    const result = await Contact.findByIdAndUpdate(
+      id,
+      { favorite },
+      { new: true }
+    );
+    if (!result) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
